be5587147fa41b938223f9a60749008b8f387c59
ChaitanyaRajeev <36008285+ChaitanyaRajeev@users.noreply.github.com>
2019-03-24 16:35:20 +0530

    Update ipl.py

--- ipl.py
+++ ipl.py
@@ -5,7 +5,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 driver = webdriver.Chrome(executable_path="E:/Software/chromedriver.exe")
 driver.get("https://www.hotstar.com/sports/cricket/vivo-ipl-2019/kolkata-knight-riders-vs-sunrisers-hyderabad-m189953/live-streaming/2001710506?lang=eng")
-time.sleep(1000)
+
 driver.delete_all_cookies()
 
 
